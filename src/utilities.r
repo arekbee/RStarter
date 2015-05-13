0f1aab11af122b90e17d231a7d1513b13dcbd631
Arkadiusz Beer <arekbee@pc9.home>
2015-05-14 01:26:28 +0200

    adding new func. iterate by day

--- src/utilities.r
+++ src/utilities.r
@@ -3,10 +3,17 @@ test.package  <- function(packageName){
     install.packages(packageName)
     library(packageName, character.only = TRUE)
   }
-}​
+}
 
 
 removeNullFromList  <- function(lst){
   lst[sapply(lst, is.null)] <- NULL
 }
 
+
+iterateByDay  <- function(fromDate, toDate){
+  daySeq <- seq(as.Date("2015/4/2"), as.Date("2016/1/1"), "day")
+  df <- data.frame(daySeq, iter, "Test2")
+  colnames(df) <- c("date", "iter", "t2")
+  return(df)
+}
